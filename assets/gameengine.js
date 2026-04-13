3502c1ac3c9b7381294d73db633995531abf6f63
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-13 11:15:34 -0700

    Define button and character classes in game engine
    Added classes for buttons, person, and interactive NPC.

--- assets/gameengine.js
+++ assets/gameengine.js
@@ -1,1 +1,4 @@
-
+class iframeButton{}//a button that loads a popup to a diffirent html page 
+class simpleButton{}// a button that bdisplays simple varibles
+class person{}//the persons health, inventory
+class interactiveNPC{}//a child of person,m when interacted with, it can give inventory based of conditions and can take from the player too
