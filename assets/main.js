be8fa2549463dfd8ad2ad77b3073653322b8981a
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-14 21:28:22 -0700

    Add soldier class with health and attack methods

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,24 @@
-
+class soilder{
+  constructor(health,power){
+    let health=health;
+    let power=power;
+    
+  }
+  function doDamage(damage){
+    this.health-=damage;
+  }
+  function attack(player){
+    if(this.health>0){
+      return player.damaged(power)
+    } else{
+      return player
+    }
+  }
+  function throwMyName(id){
+    if(this.health>0){
+      return `soilder ${id} has ${this.health}`
+    } else{
+      return `soilder ${id} is dead`
+    }
+  }
+}
