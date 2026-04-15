44053ea46bf40ccabc964a916a6f68e779a09a54
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-15 13:06:34 -0700

    Update player class and add world class structure
    Refactor player class and introduce world class with search method.

--- assets/engine_inter.js
+++ assets/engine_inter.js
@@ -23,7 +23,7 @@ class soilder{
     }
   }
 }
-class player{
+class player{//this stores the health information and inventory
   constructor(){
     let stats=[100,100,100,false,[],[],[],""];//health, power, magic, use magic, claim list, inventory, burn list, location
   }
@@ -37,7 +37,7 @@ class player{
     }
   }
   function goto(world,instruction){
-    this.stats=world.search(this);
+    this.stats=world.search(this,instruction);
   }
   function burn(){
     if(this.stats[3]){
@@ -45,5 +45,14 @@ class player{
     };
     return 0;
   }
-  
+}
+class world{//world has the ability to edit the output and is like GM
+  constructor(json_parse){
+    let time_attributes=[];//the attributes for the timeline
+    let enemies=[];//enemy list
+    let univer=json_parse;
+  }
+  function search(player,instruction){
+    this.univer.//#####WIP
+  }
 }
