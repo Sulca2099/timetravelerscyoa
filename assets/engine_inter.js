bb50491f3a60e676676660f98f7a761a61b9ba09
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-16 12:50:07 -0700

    Add handling for 'claim' in possible_rooms

--- assets/engine_inter.js
+++ assets/engine_inter.js
@@ -90,6 +90,16 @@ class world{//world has the ability to edit the output and is like GM
           if("claim" in get_here){
             player[4].push(get_here);
             player[3].push(get_here);
+            break;
+          }
+        }
+      }
+      for(let get_here of possible_rooms.gotow){
+        if(get_here===get_item){
+          if("claim" in get_here){
+            player[4].push(get_here);
+            player[3].push(get_here);
+            break;
           }
         }
       }
