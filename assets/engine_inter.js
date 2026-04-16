259ff01a4ff11c3c4c753a55fd87879fb5d6d193
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-15 18:09:31 -0700

    bug fixes

--- assets/engine_inter.js
+++ assets/engine_inter.js
@@ -16,17 +16,17 @@ class soilder{
     let power=power;
     
   }
-  function doDamage(damage){
+  doDamage(damage){
     this.health-=damage;
   }
-  function attack(player){
+  attack(player){
     if(this.health>0){
       return player.damaged(power)
     } else{
       return player
     }
   }
-  function throwMyName(id){
+  throwMyName(id){
     if(this.health>0){
       return `soilder ${id} has ${this.health}`
     } else{
@@ -38,25 +38,26 @@ class player{//this stores the health information and inventory
   constructor(){
     let stats=[100,100,100,false,[],[],[],""];//health, power, magic, use magic, claim list, inventory, burn list, location
   }
-  function damaged(damage){
+  damaged(damage){
     this.health-=damage;
     if(this.health<0){
-      document.getElementById('').innerText=`you have died!`;//the main text area
+      document.getElementById('').innerText="you have died!";//the main text area
       this.stats[6]="";//the time machine room
       this.stats[0]=100;
       return 0;
     }
+    return 0;
   }
-  function goto(world,instruction){
+  goto(world,instruction){
     this.stats=world.search(this,instruction);
   }
-  function burn(){
+  burn(){
     if(this.stats[3]){
       this.stats[5].push(this.stats[6]);
     };
     return 0;
   }
-}
+};
 class world{//world has the ability to edit the output and is like GM
   constructor(json_parse){
     let time_attributes=[];//the attributes for the timeline
@@ -64,7 +65,7 @@ class world{//world has the ability to edit the output and is like GM
     let univer=json_parse;
     let possible_rooms;
   }
-  function search(player,instruction){
+  search(player,instruction){
     this.possible_rooms=this.univer[player[6]][attributify(time_attribute)];
     if(instruction in ["north","south","east","west"]){
       return player[6]=possible_rooms[instruction];
@@ -73,7 +74,7 @@ class world{//world has the ability to edit the output and is like GM
       for(let look of possible_rooms["look"]){}
     }
   }
-  function readToMe(){
+  readToMe(){
    document.getElementById('').innerText+=this.possible_rooms.description;//enter main areas id here
   }
 }
