26fb4e1b48fb9cf3b83e979d7c9a4ddfb7a116b8
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-19 13:05:30 -0700

    Import engine_inter.js and refine class comments
    Added import statement for engine_inter.js and updated comments for clarity.

--- assets/gameengine.js
+++ assets/gameengine.js
@@ -1,4 +1,4 @@
-
+import maingame from './engine_inter.js';
 class iframeButton{}//a button that loads a popup to a diffirent html page 
 class simpleButton{}// a button that bdisplays simple varibles
 class person{}//the persons health, inventory
