97feb83517718d9b3cb3b93554cff9d3c6bd7d4e
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-19 18:47:12 -0700

    Delete assets/gameengine.js

--- assets/gameengine.js
+++
@@ -1,6 +0,0 @@
-import * from 'engine_inter.js';
-class iframeButton{}//a button that loads a popup to a diffirent html page 
-class simpleButton{}// a button that bdisplays simple varibles
-class person{}//the persons health, inventory
-class interactiveNPC{}//a child of person,m when interacted with, it can give inventory based of conditions and can take from the player too
-maingame(document);
