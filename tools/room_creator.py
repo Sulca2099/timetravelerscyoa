312af2843b85024eaacde3dc11f66081dec29929
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-20 11:47:44 -0700

    Add room_creator.py for creating rooms and objects
    Implement a room creator tool for modding purposes.

---
+++ tools/room_creator.py
@@ -0,0 +1,18 @@
+print("Public domain, NOT APART OF GAME\rMEANT FOR MODDING PURPOSES AND EASE OF\rUSE.")
+description=input("enter description here: ")
+directions=[]#without dependencies, with dependencies
+class gotoobj():
+  def __init__(self,name,type,dependenccies):
+    self.name=name
+    self.type=type
+    self.specs=[]
+    self.dependencies=dependencies
+  def runquestions(self):
+    if type==0:
+      #if:
+      pass
+while(True):
+  dire=gotoobj(input("name: "),input("type 0-room 1-object: ",input("dependencies? y/n: "))
+  dire.runquestions()
+  
+      
