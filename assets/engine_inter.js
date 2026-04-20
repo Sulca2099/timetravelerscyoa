48b630072188f7dd5cc85328657fed979ae92687
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-19 18:45:59 -0700

    Add classes for buttons and character interactions
    Added new classes for UI buttons and character management.

--- assets/engine_inter.js
+++ assets/engine_inter.js
@@ -171,3 +171,8 @@ export function maingame(document){
     //crude
   }
 }
+class iframeButton{}//a button that loads a popup to a diffirent html page 
+class simpleButton{}// a button that bdisplays simple varibles
+class person{}//the persons health, inventory
+class interactiveNPC{}//a child of person,m when interacted with, it can give inventory based of conditions and can take from the player too
+maingame(document);
