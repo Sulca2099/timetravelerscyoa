bb37fde3bb906aa8c62d03d3f7292d71a415e6d6
sulca2099 <text 408-478-5483>
2026-04-21 16:52:03 -0700

    clarifing AI

--- assets/engine_inter.js
+++ assets/engine_inter.js
@@ -167,7 +167,7 @@ function maingame(document){
   let you=new the_player();
   let command="";
   let docont=true;
-  const wait1sec = s => new Promise(resolve=>setTimeout(resolve,s*1000));
+  const wait1sec = s => new Promise(resolve=>setTimeout(resolve,s*1000));//with AI help
   while(true){
     while(docont){
       docont=!(alertwithbuttons.buttonused);
